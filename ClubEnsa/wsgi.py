d9ece99b138935dfdccc71c992e52e87fe000f6f
iliass06 <123456789+iliass06@users.noreply.github.com>
2026-04-27 23:09:03 +0100

    vercel settings2

--- ClubEnsa/wsgi.py
+++ ClubEnsa/wsgi.py
@@ -7,10 +7,27 @@ For more information on this file, see
 https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
-import os
+#import os
 
+#from django.core.wsgi import get_wsgi_application
+
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ClubEnsa.settings")
+
+#application = get_wsgi_application()
+
+
+import os
+import sys
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ClubEnsa.settings")
+# Ajoute le dossier racine au PATH pour que Python trouve les apps (authentification, clubs, etc.)
+path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+if path not in sys.path:
+    sys.path.insert(0, path)
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ClubEnsa.settings')
 
 application = get_wsgi_application()
+
+# Pour Vercel
+app = application
